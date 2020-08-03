b51cbc772c30f5b76af42efdd8876b1abb29fdd3
somealeksandr <omelkovets69@gmail.com>
2020-08-03 07:37:25 +0300

    register via firebase

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -93,7 +93,7 @@ export default {
     agree: {checked: v => v}
   },
   methods: {
-    submitHandler() {
+    async submitHandler() {
       if (this.$v.$invalid) {
         this.$v.$touch()
         return
@@ -104,9 +104,10 @@ export default {
         password: this.password,
         name: this.name
       }
-
-      this.$router.push('/')
-
+      try {
+        await this.$store.dispatch('register', formData)
+        this.$router.push('/')
+      } catch (e) {}
     }
   }
 }
