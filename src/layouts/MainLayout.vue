36ab1670732af8ac60fa5b3b0d06d7647ad8b7ed
somealeksandr <omelkovets69@gmail.com>
2020-08-29 18:24:14 +0300

    record table

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -12,7 +12,11 @@
       </main>
 
       <div class="fixed-action-btn">
-        <router-link class="btn-floating btn-large blue" to="/record">
+        <router-link
+          class="btn-floating btn-large blue"
+          to="/record"
+          v-tooltip="'Создать новую запись'"
+        >
           <i class="large material-icons">add</i>
         </router-link>
       </div>
@@ -23,23 +27,35 @@
 <script>
 import Navbar from "@/components/app/Navbar";
 import Sidebar from "@/components/app/Sidebar";
+import messages from "@/utils/messages";
 
 export default {
   name: "main-layout",
   data: () => ({
     isOpen: true,
-    loading: true
+    loading: true,
   }),
   async mounted() {
     if (!Object.keys(this.$store.getters.info).length) {
       await this.$store.dispatch("fetchInfo");
     }
 
-    this.loading = false
+    this.loading = false;
   },
   components: {
     Navbar,
     Sidebar,
+  },
+  computed: {
+    error() {
+      return this.$store.getters.error;
+    },
+  },
+  watch: {
+    error(fbError) {
+      console.log(fbError);
+      this.$error(messages[fbError.code] || "Что-то пошло не так");
+    },
   },
 };
 </script>
